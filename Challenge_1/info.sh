16a38c6283925e2a55bdfc49e50b8d6191e9c291
LuongDuyDat <luongduydat123@gmail.com>
2023-12-25 09:25:32 +0700

    finish task 1

--- Challenge_1/info.sh
+++ Challenge_1/info.sh
@@ -27,7 +27,7 @@ cpuarchitecutre=$(cut -c 15- <<< $(lscpu | grep "Architecture" | tr -s ' '));
 ram=$(getRam);
 free_storage=$(getStorageFreeSpace);
 
-ip_list=$(ip a | grep "inet");
+ip_list=$(ip a | grep "inet" | sed 's/^[[:space:]]*//');
 
 user_list=$(cut -d: -f1 /etc/passwd | sort);
 
@@ -37,14 +37,18 @@ open_ports=$(netstat -tulpn);
 
 d_list=$(find / -type d -perm -o=w 2>/dev/null);
 
+dpkg_list=$(dpkg -l);
+apt_list=$(apt list --installed);
+snap_list=$(snap list);
+
 echo "[Thong tin he thong]";
 echo '';
-echo $name;
-echo $osname;
-echo $cpuname;
-echo $cpuarchitecutre;
-echo $ram;
-echo $free_storage;
+echo "Ten may: $name";
+echo "Ten ban phan phoi: $osname";
+echo "Ten CPU: $cpuname";
+echo "Thong tin CPU: $cpuarchitecutre";
+echo "Dung luong bo nho vat ly: $ram MiB";
+echo "O dia con trong: $free_storage MiB";
 echo "Danh sach dia chi ip cua he thong: ";
 printf "%s$ip_list\n";
 echo "Danh sach user tren he thong: ";
@@ -53,5 +57,11 @@ echo "Thong tin cac tien trinh đang chay voi quyen root: "
 echo "$root_processes";
 echo "Thong tin cac port dang mo: ";
 echo "$open_ports";
-echo "Danh sach cac thu muc tren he thong cho phep other có quyen ghi";
+echo "Danh sach cac thu muc tren he thong cho phep other có quyen ghi:";
 echo "$d_list";
+echo "Danh sach cac goi phan mem duoc cai dat tren he thong boi dpkg:";
+echo "$dpkg_list";
+echo "Danh sach cac goi phan mem duoc cai dat tren he thong boi apt:";
+echo "$apt_list";
+echo "Danh sach cac goi phan mem duoc cai dat tren he thong boi snap:";
+echo "$snap_list";
